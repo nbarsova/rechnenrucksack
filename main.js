09a3543e3f32917dd09970d2c97d082ea862614b
Natalia <natfast@gmail.com>
2017-08-09 10:12:34 +0300

    added styles and preview

--- main.js
+++ main.js
@@ -31,6 +31,69 @@ function EquationsGeneratorController ($q, EquationsGeneratorService)
 
   equationsGenerator.fieldSize=10;
 
+  equationsGenerator.advancedComplexity= {value: 2,
+                                          complexity: 25,
+                                          equationsAmount: 8,
+                                          generateEquations: true,
+                                          fieldSize: 10 };
+  equationsGenerator.advancedComplexityChange = function ()
+  {
+    console.log("Changing complexity " +equationsGenerator.advancedComplexity.value);
+
+    switch (equationsGenerator.advancedComplexity.value)
+    {
+      case ('0'):
+        equationsGenerator.advancedComplexity.complexity = 10;
+        equationsGenerator.complexity=10;
+        equationsGenerator.equationsAmount = 6;
+        equationsGenerator.advancedComplexity.equationsAmount = 6;
+        equationsGenerator.fieldSize=5;
+        equationsGenerator.advancedComplexity.fieldSize = 5;
+        equationsGenerator.advancedComplexity.generateEquations = false;
+        for (var i = 0; i < equationsGenerator.operations.length; i++)
+        {
+        if ((equationsGenerator.operations[i].code === '*')||(equationsGenerator.operations[i].code === ':'))
+          {
+            equationsGenerator.operations[i].selected = false;
+          }
+        }
+        break;
+    case ('1'):
+        equationsGenerator.advancedComplexity.complexity = 10;
+        equationsGenerator.complexity=10;
+        equationsGenerator.equationsAmount = 6;
+        equationsGenerator.advancedComplexity.equationsAmount = 6;
+        equationsGenerator.fieldSize=5;
+        equationsGenerator.advancedComplexity.fieldSize = 5;
+        equationsGenerator.advancedComplexity.generateEquations = true;
+        for (var i = 0; i < equationsGenerator.operations.length; i++)
+        {
+        if ((equationsGenerator.operations[i].code === '*')||(equationsGenerator.operations[i].code === ':'))
+          {
+            equationsGenerator.operations[i].selected = false;
+          }
+        }
+        break;
+    case ('2'):
+        equationsGenerator.advancedComplexity.complexity = 25;
+        equationsGenerator.complexity=25;
+        equationsGenerator.equationsAmount = 8;
+        equationsGenerator.advancedComplexity.equationsAmount = 8;
+        equationsGenerator.fieldSize=10;
+        equationsGenerator.advancedComplexity.fieldSize = 10;
+        equationsGenerator.advancedComplexity.generateEquations = true;
+
+    case ('3'):
+        equationsGenerator.advancedComplexity.complexity = 99;
+        equationsGenerator.complexity=99;
+        equationsGenerator.equationsAmount = 10;
+        equationsGenerator.advancedComplexity.equationsAmount = 10;
+        equationsGenerator.fieldSize=10;
+        equationsGenerator.advancedComplexity.fieldSize = 10;
+        equationsGenerator.advancedComplexity.generateEquations = true;
+      }
+  }
+
   equationsGenerator.createEquations = function ()
   {
     var selectedOps=[];
@@ -79,6 +142,72 @@ function EquationsGeneratorController ($q, EquationsGeneratorService)
       }
       };
 
+
+    equationsGenerator.initializeExample = function ()
+    {
+      console.log("Initializing sample map");
+      var canvas = document.getElementById("treasureMapExample");
+
+      var context = canvas.getContext("2d");
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      context.strokeStyle = '#888888';
+      context.lineWidth = 1;
+
+      for (var ik=40; ik<=220; ik+=40)
+          {
+            context.beginPath();
+            context.moveTo(ik,15);
+            context.lineTo(ik,205);
+            context.stroke();
+          }
+
+        for (var j=40; j<=220; j+=40)
+          {
+            context.beginPath();
+            context.moveTo(15, j);
+            context.lineTo(205, j);
+            context.stroke();
+          }
+
+          var image = new Image();
+          image.src='img/stones.png';
+          image.onload=function() {
+          context.drawImage(this,65,65);
+    }
+
+      context.strokeStyle = 'blue';
+      context.lineWidth = 2;
+      context.beginPath();
+      context.moveTo(200, 162);
+      context.lineTo(80, 162);
+      context.stroke();
+      context.moveTo(86, 156);
+      context.lineTo(80, 162);
+      context.stroke();
+      context.moveTo(86, 168);
+      context.lineTo(80, 162);
+      context.stroke();
+      context.moveTo(78, 162);
+      context.lineTo(78, 82);
+      context.stroke();
+      context.moveTo(72, 90);
+      context.lineTo(78, 82);
+      context.stroke();
+      context.moveTo(84, 90);
+      context.lineTo(78, 82);
+      context.stroke();
+
+      context.font = "20px PT Sans";
+      context.fillText("Под каким камнем клад? ",220,20);
+      context.font = "18px PT Sans";
+      context.fillText("1 + 2 = _  шага налево ",220,60);
+      context.fillText("6 - 4 = _  шага вверх", 220,90);
+      context.font = "30px  Indie Flower";
+      context.fillStyle = "blue";
+      context.fillText("3", 275, 60);
+      context.fillText("2", 275, 90);
+  }
 
     equationsGenerator.createCanvas = function (targetCoordinates)
     {
--- to.do
+++ to.do
@@ -1,5 +1,11 @@
 TODO:
 
+- Advanced complexity control:
+   0 - no equations; steps: 4, 6, 8; fieldSize: 5 or 10
+   1 - equations with + and -; numbers up to 10; steps: 6, 8 or 10; fieldSize: 5 or 10
+   2 - equations with +, -, *, :; numbers up to 25; steps 6, 8, 10; fieldSize: 10
+   3 - equations with +,-,*,:; numbers up to 100; steps 8, 10, 12, fieldSize: 10.
+   
 - на первой странице - фрагмент карты с линиями, пример выполнения задания
 - вариант для самых маленьких без примеров, просто провести линии
 - вариант для нечитающих со стрелками
