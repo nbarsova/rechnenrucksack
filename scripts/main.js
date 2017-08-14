448ba9e4a157628b0d79406d0304d7a6016fbf69
Natalia <natfast@gmail.com>
2017-08-14 14:25:32 +0300

    printService error correction

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ angular.module ('EquationsGenerator', [])
 .controller ('EquationsGeneratorController', EquationsGeneratorController)
 .service('EquationsGeneratorService', EquationsGeneratorService)
 .service('ArithmeticService', ArithmeticService)
-.service('PrintService', PrintService)
 .service('TreasureMapDrawingService', TreasureMapDrawingService)
+.service('PrintService', PrintService)
 .directive('sampleTreasureMap', SampleTreasureMap);
 
 })();
