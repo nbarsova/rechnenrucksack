c99de5b135bc1ee20f00ef832902c425372f725e
Natalia Barsova <natfast@gmail.com>
2024-06-06 12:05:29 +0200

    switched to hashrouter, for the site to work on github pages

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import {IntlProvider} from "react-intl";
 import {deMessagesJSON, enMessagesJson, ruMessagesJSON} from "../../messages/messages";
