b3e87799b3e79f580b85432fe315908edc582545
Natalia Barsova <natfast@gmail.com>
2024-07-25 11:12:19 +0200

    Revert "clean up"
    This reverts commit 0e069a3fd0f4c0204bd17b23c8be0d8af11abc13.

---
+++ src/components/app/puzzles.tsx
@@ -0,0 +1,43 @@
+import {FormattedMessage} from "react-intl";
+import TreasureHunt from "../treasure/TreasureHunt";
+import SecretMessage from "../secret/SecretMessage";
+
+// @ts-ignore
+import monster from '../../img/monsterP.png';
+// @ts-ignore
+import map from '../../img/mapP.png';
+// @ts-ignore
+import secret from '../../img/secretP.png';
+
+export enum puzzleKeys  {
+    TREASURE_PUZZLE_KEY='treasure',
+    SECRET_CODE_PUZZLE_KEY='secretCode',
+    MONSTER_PUZZLE_KEY='monster'
+}
+
+export const puzzles = [{
+        key: puzzleKeys.TREASURE_PUZZLE_KEY,
+        name: <FormattedMessage
+            id="treasureMap"
+        defaultMessage="Treasure map"/>,
+        printTitle: <FormattedMessage id='worksheetTitle' />,
+        component: <TreasureHunt/>,
+        thumbnail: map
+    },
+     {
+        key: puzzleKeys.SECRET_CODE_PUZZLE_KEY,
+        name: <FormattedMessage
+            id="secretCode" defaultMessage="Secret map"/>,
+        printTitle: <FormattedMessage id='secretCodeTitle' />,
+        component: <SecretMessage/>,
+        thumbnail: secret
+    } /*,
+     {
+        key: puzzleKeys.MONSTER_PUZZLE_KEY,
+        name: <FormattedMessage
+            id="lockTheMonster" defaultMessage="Lock the Monster"/>,
+        component: <LockMonster/>,
+        thumbnail: monster,
+        printTitle: <FormattedMessage id='lockMonsterTitle' />,
+    } */
+];
--- src/svg/HardGrid.tsx
+++ src/svg/HardGrid.tsx
@@ -1,4 +1,4 @@
-
+import * as React from "react"
 const SvgComponent = (props:any) => (
     <svg xmlns="http://www.w3.org/2000/svg" {...props}>
         <path
